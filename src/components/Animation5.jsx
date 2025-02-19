c0192525c673a104fd368b75a22e09b1dee30c97
peatf <peathefeary@gmail.com>
2025-02-18 16:25:23 -0800

    Add files via upload

---
+++ src/components/Animation5.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ReactP5Wrapper } from "react-p5-wrapper";
+
+const Animation5 = () => {
+  // Future import once uploaded to GitHub:
+  // import p5Animation from '../meditationanimations/slide5limitlessselfprotocolanimationscript.jsx';
+
+  const sketch = (p5) => {
+    p5.setup = () => {
+      p5.createCanvas(400, 400);
+      p5.background(0);
+    };
+
+    p5.draw = () => {
+      p5.fill(255);
+      p5.ellipse(p5.width / 2, p5.height / 2, 50, 50);
+    };
+  };
+
+  return <ReactP5Wrapper sketch={sketch} />;
+};
+
+export default Animation5;
