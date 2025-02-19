cbe017afcaf904cd7525f5a1979577a411f0fb7b
peatf <peathefeary@gmail.com>
2025-02-18 18:15:42 -0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: false, // 🚨 Disables minification for better debugging
+};
+
+module.exports = nextConfig;
