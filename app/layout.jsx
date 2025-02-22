f41e0cb61b62f1625d547365e6a40ebdb4f935f6
peatf <peathefeary@gmail.com>
2025-02-22 10:43:54 -0800

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,8 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, 
+  user-scalable=no, maximum-scale=1.0">
         <title>Meditation Console</title>
       </head>
       <body>{children}</body>
